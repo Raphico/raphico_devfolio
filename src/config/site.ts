f5f06c3b278250be4b5fb7fd06a6a1239a5203c3
Raphael Awoyinfa <raphicogit@gmail.com>
2024-03-07 14:17:42 +0100

    feat: add site header

---
+++ src/config/site.ts
@@ -0,0 +1,11 @@
+export const siteConfig = {
+  name: "Raphico",
+  description:
+    "Welcome to the digital realm of Raphael, a passionate developer dedicated to crafting seamless solutions for the web. Explore Raphael's projects, skills, and interests in web development, gaming, and anime. Connect with Raphael to collaborate and innovate together.",
+  url: "http://raphico.vercel.app",
+  ogImage: "https://raphico.vercel.app/og.png",
+  links: {
+    twitter: "https://twitter.com/Raphico_OA",
+    github: "https://github.com/Raphico",
+  },
+}
